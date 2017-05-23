1fc584ebe285c6b88dd223b2094cb89d73602a34
Georgi Nikolov <gnikolov@sfu.ca>
2017-05-23 11:42:58 -0700

    Add redirect from '/' to '/messages'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
@@ -15,6 +15,7 @@ class App extends Component {
         </div>
         <Router>
           <div>
+            <Redirect from='/' to='/messages'/>
             <Route path='/messages' component={MessagesList}/>
           </div>
         </Router>
